216c040a3bb0f40851caae384d83cdac8335b404
Pinont <contact@pinont.me>
2026-08-28 17:58:09 +0000

    test: add MockBukkit smoke tests + un-final main class for testability

--- src/main/java/com/github/pinont/devtool/DevTool.java
+++ src/main/java/com/github/pinont/devtool/DevTool.java
@@ -2,7 +2,7 @@ package com.github.pinont.devtool;
 
 import com.github.pinont.singularitylib.plugin.CorePlugin;
 
-public final class DevTool extends CorePlugin {
+public class DevTool extends CorePlugin {
 
     @Override
     public void onPluginStart() {
